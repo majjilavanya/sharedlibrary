52b24563fdc43ee744a48f6daf9dd6dac4a30d4a
majjilavanya <152900818+majjilavanya@users.noreply.github.com>
2023-12-22 16:11:21 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,7 @@ def mavenBuild()
 }
 def deployTomcat(jobname,ip,context)
 {
-   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}mcat9/webapps/${context}.war"
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
 
